84276ed9667ff70447cb3a1ee6a35e8d245e3c9a
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-12-03 15:57:02 -0500

    Se crea la pagina general de control

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Login } from './Auth/Login';
+export { default as Dashboard } from './Dashboard';
+export { default as RegistroColaboradores } from './Colaboradores/Registro';
+export { default as TablaColaboradores } from './Colaboradores/Tabla';
+export { default as CalendarioOnboardings } from './Onboarding/Calendario';
+export { default as AlertasCorreo } from './Notificaciones/Alertas';
+export { default as NotFound } from './NotFound';
