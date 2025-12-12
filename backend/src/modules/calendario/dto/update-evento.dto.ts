500432dae2d2c66498599ceaa17976455f054b02
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-12-12 18:30:17 -0500

    Se cambia el tipo de dato en la fecha

--- backend/src/modules/calendario/dto/update-evento.dto.ts
+++ backend/src/modules/calendario/dto/update-evento.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEventoDto } from './create-evento.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateEventoDto extends PartialType(CreateEventoDto) { }
